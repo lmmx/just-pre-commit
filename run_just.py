c032395b31e24ba77f94b4fc9cb8c7e0e827bf29
Louis Maddox <louismmx@gmail.com>
2025-04-24 01:41:31 +0100

    feat: try to export rest of bin dir to PATH

--- run_just.py
+++ run_just.py
@@ -8,9 +8,18 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Path to the bundled just binary symlink
 just_path = os.path.join(script_dir, "bin", "just")
+just_dir = os.path.dirname(just_path)
 
-# Execute the bundled just binary with all passed arguments
-result = subprocess.run([just_path] + sys.argv[1:])
-print(result.stdout)
-print(result.stderr, sys.stderr)
+# Prepend that directory to PATH so "just" can be found by name
+os.environ["PATH"] = just_dir + os.pathsep + os.environ.get("PATH", "")
+
+# Now run "just" (it’ll resolve to your bundled one)
+# and inherit stdout/stderr so output flows straight through
+result = subprocess.run(
+    ["just"] + sys.argv[1:],
+    stdout=sys.stdout,
+    stderr=sys.stderr,
+)
+
+# Exit with whatever code "just" returned
 sys.exit(result.returncode)
